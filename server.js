1be01fe02a56281a5cdea4f04a4733543240b8ec
ElhamNawabi <nawabi.elham@gmail.com>
2022-04-20 19:01:58 -0400

    first commit

--- server.js
+++ server.js
@@ -17,6 +17,62 @@ app.use(express.json());
 app.use(cors());
 
 /* TODO Add Your Routes Here */
+app.post("api/user/register", (req, res) =>{
+    userService.registerUser(req.body)
+    .then(res =>{
+        res.json({message: res})
+    })
+    .catch(err =>{
+        res.status(422).json({message: err})
+    })
+})
+
+
+app.post("api/user/login", (req, res) =>{
+    userService.checkUser(req.body)
+    .then(user =>{
+        var payload = {
+            _id: user._id,
+            userName: user.userName
+        }
+        var token = jwt.sign(payload, jwtOptions.secretOrKey)
+        res.json({message: "Successfully Logged In!", token: token})
+    })
+    .catch(err =>{
+        res.status(422).json({message: err})
+    })
+})
+
+app.get("api/user/favourites", passport.authenticate("jwt"), {session: false}), (req, res) =>{
+    userService.getFavourites(req.user._id)
+    .then(item =>{
+        res.json(item)
+    })
+    .catch(err =>{
+        res.status(402).json({message: err})
+    })
+}
+
+app.put("api/user/favourites/:id", passport.authenticate("jwt"), {session: false}), (req, res) =>{
+    userService.addFavourite(req.user._id, req.params.id)
+    .then(item =>{
+        res.json(item)
+    })
+    .catch(err =>{
+        res.status(402).json({message: err})
+    })
+}
+
+app.delete("api/user/favourites/:id", passport.authenticate("jwt"), {session: false}), (req, res) =>{
+    userService.removeFavourite(req.user._id, req.params.id)
+    .then(item =>{
+        res.json(item)
+    })
+    .catch(err =>{
+        res.status(402).json({message: err})
+    })
+}
+
 
 userService.connect()
 .then(() => {
