35d7fc5c76fdf5aad92224b93e78e9ef9b20f1d1
ElhamNawabi <nawabi.elham@gmail.com>
2022-04-20 19:28:14 -0400

    commit

--- server.js
+++ server.js
@@ -13,10 +13,40 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+jwtOptions.secretOrKey = JWT_SECRET;
+
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        // The following will ensure that all routes using 
+        // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values 
+        // that matches the request payload data
+        next(null, { _id: jwt_payload._id, 
+            userName: jwt_payload.userName, 
+            fullName: jwt_payload.fullName, 
+            role: jwt_payload.role }); 
+    } else {
+        next(null, false);
+    }
+});
+
+// tell passport to use our "strategy"
+passport.use(strategy);
+
+// add passport as application-level middleware
+app.use(passport.initialize());
+
 app.use(express.json());
 app.use(cors());
 
 /* TODO Add Your Routes Here */
+
 app.post("api/user/register", (req, res) =>{
     userService.registerUser(req.body)
     .then(res =>{
@@ -35,7 +65,7 @@ app.post("api/user/login", (req, res) =>{
             _id: user._id,
             userName: user.userName
         }
-        var token = jwt.sign(payload, jwtOptions.secretOrKey)
+        var token = jwt.sign(payloa, jwtOptions.secretOrKey)
         res.json({message: "Successfully Logged In!", token: token})
     })
     .catch(err =>{
@@ -72,7 +102,6 @@ app.delete("api/user/favourites/:id", passport.authenticate("jwt"), {session: fa
         res.status(402).json({message: err})
     })
 }
-
 
 userService.connect()
 .then(() => {
