f6bc07bdf67913ac4b7422377b2ef602af021993
ElhamNawabi <nawabi.elham@gmail.com>
2022-04-20 22:34:31 -0400

    test

--- server.js
+++ server.js
@@ -13,94 +13,78 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(cors());
+
 var ExtractJwt = passportJWT.ExtractJwt;
 var JwtStrategy = passportJWT.Strategy;
 
 var jwtOptions = {};
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-jwtOptions.secretOrKey = JWT_SECRET;
+
+jwtOptions.secretOrKey = process.env.JWT_SECRET ;
 
 var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
     console.log('payload received', jwt_payload);
-
     if (jwt_payload) {
-        // The following will ensure that all routes using 
-        // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values 
-        // that matches the request payload data
         next(null, { _id: jwt_payload._id, 
-            userName: jwt_payload.userName 
-        }); 
+            userName: jwt_payload.userName}); 
     } else {
         next(null, false);
     }
 });
 
-// tell passport to use our "strategy"
-passport.use(strategy);
-
-// add passport as application-level middleware
-app.use(passport.initialize());
-
-app.use(express.json());
-app.use(cors());
-
 /* TODO Add Your Routes Here */
-
-app.post("api/user/register", (req, res) =>{
+app.post("/api/user/register", (req, res) => {
     userService.registerUser(req.body)
-    .then(res =>{
-        res.json({message: res})
-    })
-    .catch(err =>{
-        res.status(422).json({message: err})
+    .then((msg) => {
+        res.json({"User registered": msg})
+    }).catch((msg)=> {
+        res.status(422).json({"User failed to register": msg})
     })
 })
 
 
-app.post("api/user/login", (req, res) =>{
+app.post("/api/user/login", (req, res) => {
     userService.checkUser(req.body)
-    .then(user =>{
-        var payload = {
-            _id: user._id,
-            userName: user.userName
-        }
-        var token = jwt.sign(payloa, jwtOptions.secretOrKey)
-        res.json({message: "Successfully Logged In!", token: token})
-    })
-    .catch(err =>{
-        res.status(422).json({message: err})
-    })
-})
+        .then((user) => {
+            var payload = { 
+                _id: user._id,
+                userName: user.userName
+            };
+            var token = jwt.sign(payload, jwtOptions.secretOrKey);
+            res.json({ "message": "Login successful!", "token": token });
+        }).catch((msg) => {
+            res.status(422).json({ "message": msg });
+        });
+});
 
-app.get("api/user/favourites", passport.authenticate("jwt"), {session: false}), (req, res) =>{
+app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), (req, res) => {
     userService.getFavourites(req.user._id)
-    .then(item =>{
-        res.json(item)
-    })
-    .catch(err =>{
-        res.status(402).json({message: err})
-    })
-}
+        .then((msg) => {
+            res.json({ data });
+        }).catch((msg) => {
+            res.status(422).json({ "error": msg });
+        });
+});
 
-app.put("api/user/favourites/:id", passport.authenticate("jwt"), {session: false}), (req, res) =>{
-    userService.addFavourite(req.user._id, req.params.id)
-    .then(item =>{
-        res.json(item)
-    })
-    .catch(err =>{
-        res.status(402).json({message: err})
-    })
-}
+app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.addFavourite(req.user._id, id.route)
+        .then((item) => {
+            res.json(item);
+        }).catch((msg) => {
+            res.status(422).json({ "error": msg });
+        });
+});
 
-app.delete("api/user/favourites/:id", passport.authenticate("jwt"), {session: false}), (req, res) =>{
-    userService.removeFavourite(req.user._id, req.params.id)
-    .then(item =>{
-        res.json(item)
-    })
-    .catch(err =>{
-        res.status(402).json({message: err})
-    })
-}
+app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    userService.removeFavourite(req.body, id.route)
+        .then((msg) => {
+            res.json({ data });
+        }).catch((msg) => {
+            res.status(422).json({ "message": msg });
+        });
+});
 
 userService.connect()
 .then(() => {
